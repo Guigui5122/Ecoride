fce27833cbe4b1e01424cd27e740c00ece5586b3
Guigui5122 <gpelloin@gmail.com>
2025-08-12 09:56:03 +0200

    fix: Finalisation de la classe UserRepository

--- backend/src/Repositories/UserRepository.php
+++ backend/src/Repositories/UserRepository.php
@@ -6,17 +6,133 @@ use App\Models\User;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 use PDO;
 
+
+
 class UserRepository implements UserRepositoryInterface{
     
     private PDO $pdo;
+    private string $table = 'users'; 
+
 
     public function __construct(PDO $pdo) {
         $this->pdo = $pdo;
     }
+/**
+ * La fonction permet d'insérer un utilisateur ou de le mettre à jour
+ * @param $user
+ * @return bool
+ * 
+ */
 
-    public function save(User $user): ?User;
+    public function save(User $user): bool
+    {
+        try{
+        // logique pour insert ||update
+        // vérifier si l'utilisateur existe deja ou pas 
+        if($user->getId() === null){
+
+            // insérer un utilisateur en bdd 
+
+            $sql = "INSERT INTO {$this->table} (u_lastname, u_firstname, u_pseudo, u_email, u_adress, u_postal_code, u_city, u_dob, u_phone, u_password)
+            VALUES 
+            (:u_lastname,:u_firstname, :u_pseudo, :u_email, :u_adress, :u_postal_code, :u_city, :u_dob, :u_phone, :u_password)";
+
+        } else {
+            
+            // mise a jour de l'utilisateur
+            $sql = "UPDATE {$this->table} SET
+                u_lastname = :u_lastname,
+                u_firstname = :u_firstname,
+                u_pseudo = :u_pseudo,
+                u_email = :u_email,
+                u_adress = :u_adress,
+                u_postal_code = :u_postal_code,
+                u_city = :u_city,
+                u_dob = :u_dob,
+                u_phone = :u_phone,
+                u_password = :u_password
+                WHERE u_id = :u_id"; 
+        }
+
+            // Requête préparée : on cache les informations, on se protege des attaques SQL
+        $stmt = $this->pdo->prepare($sql); 
+        
+        // Remplace les params
+
+        $params = [
+                ':u_lastname' => $user->getLastname(),
+                ':u_firstname' => $user->getFirstname(),
+                ':u_pseudo' => $user->getPseudo(),
+                ':u_email' => $user->getEmail(),
+                ':u_adress' => $user->getAdress(),
+                ':u_postal_code' => $user->getPostalCode(),
+                ':u_city' => $user->getUserCity(),
+                ':u_dob' => $user->getDob(),
+                ':u_phone' => $user->getPhone(),
+                ':u_password' => password_hash($user->getPassword(), PASSWORD_ARGON2ID)
+            ];
+
+            // L'utilisateur est déjà présent dans la base
+            if($user->getId() !== null){
+                $params[':u_id'] = $user->getId();
+            }
+
+            // Exécution de la requête
+            $result = $stmt->execute($params);
+
+            if ($result && $user->getId() === null){
+                // Transmet l'id auto incrementé à PDO
+                $user->setId((int)$this->pdo->lastInsertId());
+            }
+
+        return $result;
+
+        }catch(\PDOException $error){
+        echo "La sauvegarde à échouée" . $error->getMessage();
+
+        return false;
+        }
+    }
 
 
+/**
+ * La fonction permet à l'administrateur de suspendre un compte Utilisateur
+ * @param $user 
+ * @return bool
+ */
 
 
+    public function suspendAccount(User $user): bool 
+    {
+        try{
+
+            if($user->getIsActive() === false){
+                echo "Le compte utilisateur est déjà inactif";
+                return false;
+                
+            } 
+            
+            $sql = "UPDATE {$this->table} SET u_isActive = 0 WHERE u_id = :u_id";
+            $stmt = $this->pdo->prepare($sql);
+            $result = $stmt->execute([':u_id' => $user->getId()]);
+
+            if($result){
+                $user->setU_isActive(false);
+                echo "Le compte a bien été suspendu.";
+                return true;
+            } else {
+                echo "La suspension à échouée.";
+                return false;
+            }
+            
+        }catch(PDOException $error){
+            echo "Le compte n'a pas été suspendu, veuillez réésssayer " . $error->getMessage();
+            return false;
+        } 
+    }
 }
+
+
+
+
+
